84b5cdb8b43a80cfccd03108ffabb4ad12939a7c
Joanna <112305002@g.nccu.edu.tw>
2024-12-13 14:57:36 +0800

    1213

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// Selecting all drum buttons
+
 const drumButtons = document.querySelectorAll(".drum");
 
-// Function to play the sound based on the key
+
 function playSound(key) {
     switch (key) {
         case "w":
-            new Audio("sounds/tom-1.mp3").play();
-            break;
-        case "a":
-            new Audio("sounds/tom-2.mp3").play();
-            break;
-        case "s":
-            new Audio("sounds/tom-3.mp3").play();
-            break;
-        case "d":
-            new Audio("sounds/tom-4.mp3").play();
-            break;
-        case "j":
-            new Audio("sounds/snare.mp3").play();
-            break;
-        case "k":
             new Audio("sounds/crash.mp3").play();
             break;
+        case "a":
+            new Audio("sounds/kick.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
         case "l":
-            new Audio("sounds/kick-bass.mp3").play();
+            new Audio("sounds/tom-4.mp3").play();
             break;
         default:
             console.log(key);
